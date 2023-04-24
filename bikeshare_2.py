8ddb248a49c75a7e244cda83d1b3d70c04a53d4d
Vinay Chowhan <vinay_chowhan@hotmail.com>
2023-04-23 22:36:51 -0700

    refactor: comment added to dictionary used for city and city data files

--- bikeshare_2.py
+++ bikeshare_2.py
@@ -3,6 +3,8 @@ import time
 import numpy as np
 import pandas as pd
 
+#dictonary of city and data files used
+
 CITY_DATA = {
     "chicago": "chicago.csv",
     "new york city": "new_york_city.csv",
